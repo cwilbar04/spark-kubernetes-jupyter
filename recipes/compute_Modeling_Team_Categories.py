0a8fff296f5fe4e674286b2e5502095c8bec0d64
U429930 <Christopher_Wilbar@bcbsil.com>
2021-10-13 15:35:28 -0500

    Saved recipe: IL_CONTRACT_MONITORING_DEV.compute_Modeling_Team_Categories

--- recipes/compute_Modeling_Team_Categories.py
+++ recipes/compute_Modeling_Team_Categories.py
@@ -9,8 +9,8 @@ category_ds = dataiku.Dataset("Contract_Category_Table_Latest")
 category_df = category_ds.get_dataframe()
 
 # Rev Code needs to be 4 digits with leading zeros
-category_df.loc[category_df['Code_Type'] == 'REV','Code'] =\
-    category_df.loc[category_df['Code_Type'] == 'REV','Code'].apply(lambda x: x.zfill(4))
+category_df.loc[category_df['Code_Type'] == 'RVNU','Code'] =\
+    category_df.loc[category_df['Code_Type'] == 'RVNU','Code'].apply(lambda x: x.zfill(4))
 
 # Compute recipe outputs from inputs
 
